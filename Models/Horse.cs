a9270248f2ecce557d63ec3d8969dbbb2d6c6ec4
Matthew Zwerlein <mzwerlein@my.wctc.edu>
2022-02-21 21:05:30 -0600

    first commit

---
+++ Models/Horse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractExample.Models
+{
+    public class Horse : Animal
+    {
+        public Horse(string name)
+        {
+            Name = name;
+        }
+
+        public Horse()
+        {
+            Name = "Sebastian";
+        }
+
+        public override void MakeNoise()
+        {
+            Console.WriteLine($"{Name} says nay? neh? nay");
+        }
+
+        public override void Sleep()
+        {
+            Console.WriteLine("*Snore*");
+        }
+    }
+}
